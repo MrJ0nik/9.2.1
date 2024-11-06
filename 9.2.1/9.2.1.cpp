22217d3c3b0db45a5494b3f50e3e6502ccb2f9ea
MrJ0nik <142501317+MrJ0nik@users.noreply.github.com>
2024-11-06 19:44:18 +0200

    first commit

---
+++ 9.2.1/9.2.1.cpp
@@ -0,0 +1,73 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+
+#define ABS(a) ((a) < 0 ? -(a) : (a))
+
+
+#define SQUARE(a) ((a) * (a))
+
+
+#define POWER(a, b) (pow((a), (b)))
+
+
+double calculateWFormula1(double x, double z) {
+    return MAX(x, z) * MIN(x + z, x * z);
+}
+
+
+double calculateWFormula2(double x, double z) {
+    return SQUARE(MIN(ABS(x - z), z));
+}
+
+
+#define CALCULATE_W(x, z) ((5 < (z) && (z) < 10) ? calculateWFormula1((x), (z)) : calculateWFormula2((x), (z)))
+
+
+double calculateWFormula1(double x, double z) {
+    return MAX(x, z) * MIN(x + z, x * z);
+}
+
+
+double calculateWFormula2(double x, double z) {
+    return SQUARE(MIN(ABS(x - z), z));
+}
+
+
+double calculateW(double x, double z) {
+    return CALCULATE_W(x, z);
+}
+
+
+int main() {
+    double x, z;
+
+   
+    cout << " Enter x,z: ";
+    cin >> x >> z;
+
+  
+#if 5 < z && z < 10
+#define FORMULA1
+#else
+#undef FORMULA1
+#endif
+
+
+    double w = calculateW(x, z);
+
+
+    cout << "reuslt w: " << w << endl;
+
+    return 0;
+}
+    
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,60 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "../9.2.1/9.2.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(a) ((a) < 0 ? -(a) : (a))
+#define SQUARE(a) ((a) * (a))
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestFormula1)
+        {
+           
+            double x = 6.0, z = 7.0;
+            double expected = MAX(x, z) * MIN(x + z, x * z);
+            Assert::AreEqual(expected, calculateWFormula1(x, z), 0.001);
+        }
+
+        TEST_METHOD(TestFormula2)
+        {
+           
+            double x = 4.0, z = 5.0;
+            double expected = SQUARE(MIN(ABS(x - z), z));
+            Assert::AreEqual(expected, calculateWFormula2(x, z), 0.001);
+        }
+
+        TEST_METHOD(TestCalculateW_Formula1)
+        {
+            
+            double x = 6.0, z = 7.0;
+            double expected = calculateWFormula1(x, z);
+            Assert::AreEqual(expected, calculateW(x, z), 0.001);  
+        }
+
+        TEST_METHOD(TestCalculateW_Formula2_LowZ)
+        {
+            
+            double x = 4.0, z = 5.0;
+            double expected = calculateWFormula2(x, z);
+            Assert::AreEqual(expected, calculateW(x, z), 0.001);  
+        }
+
+        TEST_METHOD(TestCalculateW_Formula2_HighZ)
+        {
+            
+            double x = 4.0, z = 10.0;
+            double expected = calculateWFormula2(x, z);
+            Assert::AreEqual(expected, calculateW(x, z), 0.001);  
+        }
+    };
+}
